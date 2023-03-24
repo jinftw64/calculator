3ad719d11d274df3760b8921d09f63b9d21105f4
jinftw64 <103798287+jinftw64@users.noreply.github.com>
2023-03-23 22:58:16 -0400

    Create variables for operator and operands. Create operate function using switch cases for the various operations

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ const multiply = function(a, b) {
 const divide = function(a, b) {
   return a / b
 }
+
+let firstOperand;
+let secondOperand;
+let operator;
+
+const operate = function(operator, firstNum, secondNum) {
+  switch (operator) {
+    case '+':
+      return add(firstNum, secondNum);
+    case '-':
+      return subtract(firstNum, secondNum);
+    case '*':
+      return multiply(firstNum, secondNum);
+    case '/':
+      return divide(firstNum, secondNum);
+  }
+}

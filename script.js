d26c3431984659c6b128c41f5854b15c4e78f056
jinftw46 <jin@PLATCUBAHILL16>
2023-03-27 12:01:19 -0400

    Refactor logic to update display accordingly with operator and operand inputs.

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ const divide = function(a, b) {
 let firstOperand = '';
 let secondOperand = '';
 let operator;
+let displayString = ''
 let chainingOperators = false;
 
 const operate = function(operator, firstNum, secondNum) {
@@ -34,29 +35,25 @@ const operate = function(operator, firstNum, secondNum) {
 
 const updateDisplay = function() {
   const display = document.querySelector('.display');
-  if (!operator) {
-    update1stOperand;
-  } else {
-    update2ndOperand;
-  }
-  display.textContent = string;
+  displayString = displayString.concat(this.value);
+  display.textContent = displayString;
 }
 
 const update1stOperand = function() {
-  firstOperand = firstOperand.concat(this.value);
 }
 
 const update2ndOperand = function() {
-  secondOperand = secondOperand.concat(this.value);
 }
 
 const updateOperator = function() {
   operator = this.value;
-  chainingOperators = true;
-  // add css here to highlight the current operator
+  const display = document.querySelector('.display');
+  firstOperand = display.textContent;
+  displayString = '';
+  // add css to highlight the button
 }
 
 const operands = document.querySelectorAll('.operand')
 const operators = document.querySelectorAll('.operator')
 operands.forEach(operand => operand.addEventListener('click', updateDisplay));
-operators.forEach(operator => operator.addEventListener('click',));
+operators.forEach(operator => operator.addEventListener('click', updateOperator));
